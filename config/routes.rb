610c71cca52b0664225e9516388710c55764fca0
Steven Senatori <senatori@mail.sfsu.edu>
2013-11-23 14:47:15 -0800

    a commit before switching

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ SampleApp::Application.routes.draw do
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
 
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
